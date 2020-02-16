6d55986f6f167a23ecee7021b77517d0167796ff
Matthew-Starcevic <60372329+Matthew-Starcevic@users.noreply.github.com>
2020-02-16 14:22:30 -0600

    Simplification
    Removed the error objects, as I realized in hindsight that this probably isn't the right file for them. Also removed some redundant array properties from a couple of the objects. Customized the property fields the professor provided as a starter to be something a little more amusing.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const item = {
     itemID: '02bbdbc7-e22e-4153-abd8-b5732a4ba6b5',
-    name: 'Ball cap',
-    description: 'Drake stuff',
+    name: 'One (1) Bonk to the Head',
+    description: 'https://www.youtube.com/watch?v=gwxTZaa3NgI',
     price: 19.99,
-    size: 'Large'
+    size: 'Big'
 };
 
 const firstName = 'Dude';
@@ -14,54 +14,24 @@ const customer = {
     firstName,
     lastName,
     email: `${firstName.toLowerCase()}.${lastName.toLowerCase()}@drake.edu`,
-    phoneNumber: '+15155555555',
-    customerCarts: '[B76A19B4-E5FD-4AB1-9B2E-47DC777F1655]'
+    phoneNumber: '+9999999999'
 };
 
 const cart = {
     cartID: 'B76A19B4-E5FD-4AB1-9B2E-47DC777F1655',
     cartOwnerID: '5BA738D0-E31D-49A9-AB09-AE7CCD259CAD',
     createdDate: '1-15-2020',
-    purchasedDate: '2-1-2020',
-    CartItems: '[02bbdbc7-e22e-4153-abd8-b5732a4ba6b5]'
+    purchasedDate: '2-1-2020'
 };
 
 const cartItem = {
     inCart: 'B76A19B4-E5FD-4AB1-9B2E-47DC777F1655',
     cartItemIndex: 1,
     itemIs: '02bbdbc7-e22e-4153-abd8-b5732a4ba6b5',
-    quantity: 1,
-};
-
-const itemNotFound = {
-    statusCode: 404,
-    error: 'Not Found',
-    message: 'Item not found.'
-};
-
-const customerNotFound = {
-    statusCode: 404,
-    error: 'Not Found',
-    message: 'Customer not found.'
-};
-
-const cartNotFound = {
-    statusCode: 404,
-    error: 'Not Found',
-    message: 'Cart not found.'
-};
-
-const cartItemNotFound = {
-    statusCode: 404,
-    error: 'Not Found',
-    message: 'Item not found in cart.'
+    quantity: 1
 };
 
 console.log('item', item);
 console.log('customer', customer);
 console.log('cart', cart);
 console.log('cartItem', cartItem);
-console.log('itemNotFound', itemNotFound);
-console.log('customerNotFound', customerNotFound);
-console.log('cartNotFound', cartNotFound);
-console.log('cartItemNotFound', cartItemNotFound);
