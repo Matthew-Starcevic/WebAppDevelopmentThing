15a3caf7301cbe96854baaa8eb7a5703a4d49b9b
Matthew-Starcevic <60372329+Matthew-Starcevic@users.noreply.github.com>
2020-02-23 11:01:00 -0600

    Update index.js

--- index.js
+++ index.js
@@ -24,12 +24,20 @@ const cart = {
     purchasedDate: '2-1-2020'
 };
 
-const cartItem = {
-    inCart: 'B76A19B4-E5FD-4AB1-9B2E-47DC777F1655',
-    cartItemIndex: 1,
-    itemIs: '02bbdbc7-e22e-4153-abd8-b5732a4ba6b5',
-    quantity: 1
-};
+const cartItems = [
+    {
+        inCart: 'B76A19B4-E5FD-4AB1-9B2E-47DC777F1655',
+        cartItemId: '3cac179b-8da2-4618-b80d-0afa63e8fe7b',
+        itemIs: '02bbdbc7-e22e-4153-abd8-b5732a4ba6b5',
+        quantity: 1
+    },
+    {
+        inCart: 'B76A19B4-E5FD-4AB1-9B2E-47DC777F1655',
+        cartItemId: '8f28e62d-b2cd-452d-baa4-3e52a994715e',
+        itemIs: '05dcd5c5-2453-426d-af1e-cb1cd3e33bad',
+        quantity: 7
+     }
+];
 
 console.log('item', item);
 console.log('customer', customer);
