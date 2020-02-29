3e99c678de4a26bf560e785a886e375ba5f60c69
Matthew-Starcevic <60372329+Matthew-Starcevic@users.noreply.github.com>
2020-02-29 13:42:43 -0600

    Updated customer-repository.js

--- repositories/customer-repository.js
+++ repositories/customer-repository.js
@@ -1,1 +1,25 @@
+const uuid = require('uuid');
 
+let customers = [
+  {
+    'customer_id': 'd83ff143-9f8b-445a-8d8f-b9b8fe0f9f28',
+    'first_name': 'Dude',
+    'last_name': 'Withaname',
+    'email': 'Dude.Withaname@drake.edu'
+  }
+];
+
+const selectCustomers = () => ({
+  rows: customers,
+  error: new Error(),
+  driver: 'postgres'
+});
+
+const selectCustomerByCustomerID = (customerID) => ({
+  customers.find((customer) => customer['customer_id'] === customerID);
+});
+
+module.exports = {
+  selectCustomers,
+  selectCustomerByCustomerID
+};
