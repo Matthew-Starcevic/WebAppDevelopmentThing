7cf9ca14f26c8095b28f3dca38e47747fc540f98
Matthew-Starcevic <60372329+Matthew-Starcevic@users.noreply.github.com>
2020-02-29 14:31:22 -0600

    Updated item-service.js

--- services/item-service.js
+++ services/item-service.js
@@ -1,1 +1,27 @@
+const {
+  selectItems,
+  selectItemByItemID
+} = require('../repositories/item-repository');
 
+const mapToModel = (item) => ({
+  itemID: item['item_id'],
+  name: item['name'],
+  description: item['description'],
+  price: item['price'],
+  size: item['size']
+});
+
+const getAllItems = () => {
+  const {rows} = selectItems();
+  return rows.map(mapToModel);
+};
+
+const getItemByItemID = (itemID) => {
+  const item = selectItemByItemID(itemID);
+  return mapToModel(item);
+};
+
+module.exports = {
+  getAllItems,
+  getItemByItemID
+};
