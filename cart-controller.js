9ddff210b3495a31d10e0ff95b6e18f4a7bc0c28
Matthew-Starcevic <60372329+Matthew-Starcevic@users.noreply.github.com>
2020-02-29 11:54:56 -0600

    Update cart-controller.js

--- cart-controller.js
+++ cart-controller.js
@@ -1,1 +1,39 @@
+const {
+  getAllCarts,
+  getCartByCartID
+} = require('../services/cart-service');
 
+const getCartsRoute = (server) => {
+  server.route({
+    path: '/carts',
+    method: 'GET',
+    handler: (request, h) => {
+      return getAllCarts();
+    }
+  });
+};
+
+const getCartByCartIDRoute = (server) => {
+  server.route({
+    path: '/carts/{cartId}',
+    method: 'GET',
+    handler: (request, h) => {
+      const cart = getCartByCartID(request.params.cartId);
+      
+      if (!cart) {
+        return h.response().code(404);
+      }
+      
+      return cart;
+    }
+  });
+};
+
+const initCartControllers = (server) => {
+  getCartsRoute(server);
+  getCartByCartIDRoute(server);
+};
+
+module.exports = {
+  initCartControllers
+};
