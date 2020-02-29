6529a44077c3aa04cf1c4d91ea5e6d0e59856a98
Matthew-Starcevic <60372329+Matthew-Starcevic@users.noreply.github.com>
2020-02-29 13:57:56 -0600

    Updated cart-service.js

--- services/cart-service.js
+++ services/cart-service.js
@@ -1,1 +1,33 @@
+const {
+  selectCarts,
+  selectCartByCartID,
+  selectCartsByCustomerID
+} = require('../repositories/cart-repository');
 
+const mapToModel = (cart) => ({
+  cartID: cart['cart_id'],
+  customerID: cart['customer_id'],
+  createdDate: cart['created_date'],
+  purchasedDate: cart['purchased_date']
+});
+
+const getAllCarts = () => {
+  const {rows} = selectCarts();
+  return rows.map(mapToModel);
+};
+
+const getCartByCartID = (cartID) => {
+  const cart = selectCartByCartID(cartID);
+  return mapToModel(cart);
+};
+
+const getCartsByCustomerID = (customerID) => {
+  const {rows} = selectCartsByCustomerID(customerID);
+  return rows.map(mapToModel);
+};
+
+module.exports = {
+  getAllCarts,
+  getCartByCartID,
+  getCartsByCustomerID
+};
