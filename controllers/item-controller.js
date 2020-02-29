ecc7039ac85ae983519357fe71db220ad05d4573
Matthew-Starcevic <60372329+Matthew-Starcevic@users.noreply.github.com>
2020-02-29 14:14:49 -0600

    Updated item-controller.js

--- controllers/item-controller.js
+++ controllers/item-controller.js
@@ -1,1 +1,39 @@
+const {
+  getAllItems,
+  getItemByItemID
+} = require('../services/item-service')
 
+const getItemsRoute = (server) => {
+  server.route({
+    path: '/items',
+    method: 'GET',
+    handler: (request, h) => {
+      return getAllItems();
+    }
+  });
+};
+
+const getItemByItemIDRoute = (server) => {
+  server.route({
+    path: '/items/{itemID}',
+    method: 'GET',
+    handler: (request, h) => {
+      const item = getItemByItemID(request.params.itemID);
+      
+      if (!item) {
+        return h.response().code(404);
+      }
+      
+      return item;
+    }
+  });
+};
+
+const initItemControllers = (server) => {
+  getItemsRoute(server);
+  getItemByItemIDRoute(server);
+};
+
+module.exports = {
+  initItemControllers
+};
