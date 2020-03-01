5a3b647ff7fe6863ad9e103fa338900096252323
Matthew-Starcevic <60372329+Matthew-Starcevic@users.noreply.github.com>
2020-02-29 19:46:07 -0600

    Small Change for Testing Purposes

--- repositories/cart-repository.js
+++ repositories/cart-repository.js
@@ -2,13 +2,13 @@ const uuid = require('uuid');
 
 let carts = [
   {
-    'cart_id': uuid.v4(),
+    'cart_id': '490215b7-ab53-450f-8ecf-b1c3e035c640',
     'customer_id': 'd83ff143-9f8b-445a-8d8f-b9b8fe0f9f28',
     'created_date': new Date(),
     'purchased_date': new Date()
   },
   {
-    'cart_id': uuid.v4(),
+    'cart_id': '6997ddba-abd6-4547-8398-205d4d8946e8',
     'customer_id': 'd83ff143-9f8b-445a-8d8f-b9b8fe0f9f28',
     'created_date': new Date(),
     'purchased_date': new Date()
