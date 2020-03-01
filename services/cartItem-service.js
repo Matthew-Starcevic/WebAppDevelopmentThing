a9b1b83bae2625c24226d41ae11cdf6badf12db0
Matthew-Starcevic <60372329+Matthew-Starcevic@users.noreply.github.com>
2020-02-29 20:14:21 -0600

    Updated cartItem-service.js

--- services/cartItem-service.js
+++ services/cartItem-service.js
@@ -1,1 +1,19 @@
+const {
+  selectCartItemByCartItemID
+} = require('../repositories/cartItem-repository');
 
+const mapToModel = (cartItem) => ({
+  inCart: cartItem['in_cart'],
+  cartItemID: cartItem['cart_item_id'],
+  itemIs: cartItem['item_is'],
+  quantity: cartItem['quantity']
+});
+
+const getCartItemByCartItemID = (cartItemID) => {
+  const cartItem = selectCartItemByCartItemID(cartItemID);
+  return mapToModel(cartItem);
+};
+
+module.exports = {
+  getCartItemByCartItemID
+};
