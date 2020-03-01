712e09ff31f0737c884b2d5dcc458645cda2ecdd
Matthew-Starcevic <60372329+Matthew-Starcevic@users.noreply.github.com>
2020-02-29 20:57:13 -0600

    Updated cart-controller.js
    Scratch my previous comment about not being sure if this is the correct way to implement the Show Items in Cart functionality. I understand what the pathing means now.

--- controllers/cart-controller.js
+++ controllers/cart-controller.js
@@ -3,6 +3,8 @@ const {
   getCartByCartID
 } = require('../services/cart-service');
 
+const {getCartItemsByCartID} = require('../services/cartItem-service');
+
 const getCartsRoute = (server) => {
   server.route({
     path: '/carts',
@@ -29,9 +31,27 @@ const getCartByCartIDRoute = (server) => {
   });
 };
 
+const getItemsInCartRoute = (server) => {
+  server.route({
+    path: '/carts/{CartID}/cartItems',
+    method: 'GET',
+    handler: (request, h) => {
+      const cartID = request.params.cartID;
+      const cart = getCartByCartID(cartID);
+      
+      if (!cart) {
+        return h.response().code(404);
+      }
+      
+      return getCartItemsByCartID(cartID);
+    }
+  });
+};
+
 const initCartControllers = (server) => {
   getCartsRoute(server);
   getCartByCartIDRoute(server);
+  getItemsInCartRoute(server);
 };
 
 module.exports = {
