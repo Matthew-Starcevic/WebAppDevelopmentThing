76d2a06a2c5d78ffa5c98f691616d013acc91f0a
Matthew-Starcevic <60372329+Matthew-Starcevic@users.noreply.github.com>
2020-03-22 05:35:30 -0500

    Updated Service Tests Directory

--- test/services/cart-service.test.js
+++ test/services/cart-service.test.js
@@ -5,7 +5,6 @@ const {
   getCartByCartID,
   getCartsByCustomerID
 } = require('../../services/cart-service');
-
 const {
   selectCarts,
   selectCartByCartID,
@@ -18,53 +17,50 @@ describe('getAllCarts', () => {
   let expectedFirstCart,
       expectedFirstCartID,
       expectedCustomerID;
-  
+
   beforeEach(() => {
     expectedFirstCartID = uuid.v4();
     expectedCustomerID = uuid.v4();
-    
     expectedFirstCart = {
       cartID: expectedFirstCartID,
       customerID: expectedCustomerID
     };
-    
     selectCarts.mockReturnValue({
       rows: [{
         'cart_id': expectedFirstCartID,
         'customer_id': expectedCustomerID
       }]
     });
-    
     selectCartsByCustomerID.mockReturnValue({
       rows: [{
         'cart_id': expectedFirstCartID,
         'customer_id': expectedCustomerID
       }]
     });
-    
     selectCartByCartID.mockReturnValue({
       'cart_id': expectedFirstCartID,
       'customer_id': expectedCustomerID
-    })
+    });
   });
 
   it('should get all the carts', () => {
     const actualCarts = getAllCarts();
+
     expect(selectCarts).toHaveBeenCalledTimes(1);
     expect(actualCarts).toEqual([
       expectedFirstCart
     ]);
   });
-  
   it('should get a cart by a specific cartID', () => {
     const actualCart = getCartByCartID(expectedFirstCartID);
+
     expect(selectCartByCartID).toHaveBeenCalledTimes(1);
     expect(selectCartByCartID).toHaveBeenCalledWith(expectedFirstCartID);
     expect(actualCart).toEqual(expectedFirstCart);
   });
-  
   it('should get all the carts by customerID', () => {
     const actualCarts = getCartsByCustomerID(expectedCustomerID);
+
     expect(selectCartsByCustomerID).toHaveBeenCalledTimes(1);
     expect(selectCartsByCustomerID).toHaveBeenCalledWith(expectedCustomerID);
     expect(actualCarts).toEqual([
--- test/services/customer-service.test.js
+++ test/services/customer-service.test.js
@@ -4,7 +4,6 @@ const {
   getAllCustomers,
   getCustomerByCustomerID
 } = require('../../services/customer-service');
-
 const {
   selectCustomers,
   selectCustomerByCustomerID
@@ -18,49 +17,44 @@ describe('getAllCustomers', () => {
       expectedCustomerFirstName,
       expectedCustomerLastName,
       expectedCustomerEmail;
-  
+
   beforeEach(() => {
     expectedCustomerID = uuid.v4();
     expectedCustomerFirstName = 'Dude';
     expectedCustomerLastName = 'Withaname';
     expectedCustomerEmail = 'Dude.Withaname@drake.edu';
-    
     expectedCustomer = {
       customerID: expectedCustomerID,
+      email: expectedCustomerEmail,
       firstName: expectedCustomerFirstName,
-      lastName: expectedCustomerLastName,
-      email: expectedCustomerEmail
+      lastName: expectedCustomerLastName
     };
-    
     selectCustomers.mockReturnValue({
       rows: [{
         'customer_id': expectedCustomerID,
+        'email': expectedCustomerEmail,
         'first_name': expectedCustomerFirstName,
-        'last_name': expectedCustomerLastName,
-        'email': expectedCustomerEmail
-      }];
+        'last_name': expectedCustomerLastName
+      }]
     });
-    
     selectCustomerByCustomerID.mockReturnValue({
       'customer_id': expectedCustomerID,
+      'email': expectedCustomerEmail,
       'first_name': expectedCustomerFirstName,
-      'last_name': expectedCustomerLastName,
-      'email': expectedCustomerEmail
+      'last_name': expectedCustomerLastName
     });
   });
-  
   it('should get all the customers', () => {
     const actualCustomers = getAllCustomers();
-    
+
     expect(selectCustomers).toHaveBeenCalledTimes(1);
     expect(actualCustomers).toEqual([
       expectedCustomer
     ]);
   });
-  
   it('should get a customer by a specific customerID', () => {
     const actualCustomer = getCustomerByCustomerID(expectedCustomerID);
-    
+
     expect(selectCustomerByCustomerID).toHaveBeenCalledTimes(1);
     expect(selectCustomerByCustomerID).toHaveBeenCalledWith(expectedCustomerID);
     expect(actualCustomer).toEqual(expectedCustomer);
